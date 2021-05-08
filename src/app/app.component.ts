7473a16811a98bc3196e2b1699abd47f8e2c1471
Andres Caro <andres.caro@sofka.com.co>
2021-05-08 16:46:19 -0500

    Uso de ngFor add y delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'andres']
+
+  addItem(){
+    this.items.push('nuevo item')
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1)
+  }
 }
