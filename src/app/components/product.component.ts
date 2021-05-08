7358e135bb5ec95168d1bc946181e85994e21791
Andres Caro <andres.caro@sofka.com.co>
2021-05-08 18:08:50 -0500

    Uso de Inputs y Outputs

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Product} from '../product.model'
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Product} from '../product.model';
+
 
 @Component({
     selector: 'app-product',
@@ -7,13 +8,11 @@ import { Product} from '../product.model'
 })
 export class ProductComponent {
 
-    product: Product=
-    {
-        id: '1',
-        image: 'assets/images/camiseta.png',
-        title: 'Camiseta',
-        price: 80000,
-        description: 'bla bla bla bla bla'
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    addCart(){
+        console.log('añadir al carrito');
+        this.productClicked.emit(this.product.id)
     }
-    
 }
