6105bd687f34775dd2bddda1e0b71ae0b3cdd371
Andres Caro <andres.caro@sofka.com.co>
2021-05-09 16:36:13 -0500

    Se generaron estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Product} from './product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'platzi-store';
--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -15,7 +15,9 @@ import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
-    templateUrl: './product.component.html'
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
+
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
