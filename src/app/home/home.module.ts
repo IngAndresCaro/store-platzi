aa4eb420ab07c53f356943ba38632b597ce82f50
Andres Caro <andres.caro@sofka.com.co>
2021-05-11 13:37:45 -0500

    Se paso de ser un componente home a ser un modulo

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import {BannerComponent} from './components/banner/banner.component';
+import {HomeComponent} from './home.component';
+
+import{HomeRoutingModule} from './home-routing.module';
+
+@NgModule({
+    declarations: [
+        BannerComponent,
+        HomeComponent
+    ],
+    imports: [
+        HomeComponent
+    ]
+  })
+
+  export class HomeModule { }
