ce6b71a06190f2e821dbd4394dbdf064ff72fd14
Andres Caro <andres.caro@sofka.com.co>
2021-05-12 12:12:07 -0500

    Se creo un crud para los elementos de productos

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -20,8 +20,48 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      this.product = this.producsService.getProduct(id);
+      this.fetchProduct(id);
+      //this.product = this.producsService.getProduct(id);
     });
   }
 
+  fetchProduct(id: string) {
+    this.producsService.getProduct(id)
+      .subscribe(product => {
+        this.product = product;
+      });
+  }
+
+  createProduct() {
+    const newProduct: Product = {
+      id: '100',
+      title: "nuevo desde angular",
+      image: "assets/images/hoodie.png",
+      price: 30000,
+      description: "nuevo pruducto"
+    };
+    this.producsService.createProduct(newProduct)
+      .subscribe(product => {
+        console.log(product);
+      });
+  }
+
+  updateProduct(){
+    const updateProduct: Partial<Product> = {
+      price: 55555,
+      image: "assets/images/hoodie.png",
+      description: "edicion titulo"
+    };
+    this.producsService.updateProduct('100',updateProduct)
+      .subscribe(product => {
+        console.log(product);
+      });
+  }
+
+  deleteProduct(){
+    this.producsService.deleteProduct('100')
+      .subscribe(product => {
+        console.log(product);
+      });
+  }
 }
