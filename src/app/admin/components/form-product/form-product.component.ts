77d35e929553652c1daa1d6919eebf7eb320b156
Andres Caro <andres.caro@sofka.com.co>
2021-05-13 22:06:22 -0500

    Se crea el metodo para poder subir imagenes en el form-product-componet

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { MyValidators } from './../../../utils/validators'
-
+import { AngularFireStorage } from '@angular/fire/storage';
+import { MyValidators } from './../../../utils/validators';
+import { finalize } from 'rxjs/operators'
 import { ProductsService } from './../../../core/services/products/products.service';
-
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -14,11 +15,13 @@ import { ProductsService } from './../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm()
   }
@@ -38,19 +41,35 @@ export class FormProductComponent implements OnInit {
     }
   }
 
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const name = 'images';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url => {
+            this.form.get('image').setValue(url)
+          })
+        })
+      ).subscribe();
+  }
+
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
-      image: [''],
+      image: ['', [Validators.required]],
       description: ['', [Validators.required]]
 
     })
   }
 
-  get priceField(){
+  get priceField() {
     return this.form.get('price')
   }
-
 }
