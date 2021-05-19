cbff81963c7ade255a39876532e7743e0548355e
Andres Caro <andres.caro@sofka.com.co>
2021-05-19 16:14:00 -0500

    Se genera un metodo para una percarga selectiva

--- src/app/app-routin.module.ts
+++ src/app/app-routin.module.ts
@@ -1,61 +1,65 @@
 import { NgModule } from '@angular/core';
+import { AdminGuard } from './admin.guard';
+import { LayoutComponent } from './layout/layout.component';
+import { PreloadService } from '@core/services/preload/preload.service';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { LayoutComponent } from './layout/layout.component';
 
-import { AdminGuard } from './admin.guard';
 
 const routes: Routes = [
-    {
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: LayoutComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: '/home',
-                pathMatch: 'full',
-              },
-              {
-                path: 'home',
-                loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-              },
-              {
-                path: 'products',
-                loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
-              },
-              {
-                path: 'contact',
-                loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
-              },
-              {
-                path: 'order',
-                loadChildren: () => import('./order/order.module').then(m => m.OrderModule)
-              },
-              {
-                path: 'demo',
-                loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
-              },
-        ]
-    },
-    {
-      path: 'admin',
-      canActivate: [AdminGuard],
-      loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
-    },
-    {
-      path: 'auth',
-      loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-    },
-    {
-        path: '**',
-        loadChildren: () => import('./not-found/not-found.module').then(m => m.PageNotFoundModule)
-    },
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+        data: { preload: true }
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+        data: { preload: true }
+      },
+      {
+        path: 'contact',
+        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+      },
+      {
+        path: 'order',
+        loadChildren: () => import('./order/order.module').then(m => m.OrderModule)
+      },
+      {
+        path: 'demo',
+        loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
+      },
+    ]
+  },
+  {
+    path: 'admin',
+    canActivate: [AdminGuard],
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./not-found/not-found.module').then(m => m.PageNotFoundModule)
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {
-        preloadingStrategy: PreloadAllModules
-    })],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    //preloadingStrategy: PreloadAllModules cambiamos la manera de cargar nuestros mudulos
+    preloadingStrategy: PreloadService
+  })],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
